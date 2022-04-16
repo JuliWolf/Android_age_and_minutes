ce8f84a07a786da9e234037a9a85a573d066646c
Julia Ivanova <for.julia96@gmail.com>
2022-04-16 16:22:03 +0300

    Age in minutes app

--- app/src/main/java/com/example/android_base_ui/MainActivity.kt
+++ app/src/main/java/com/example/android_base_ui/MainActivity.kt
@@ -1,25 +1,60 @@
 package com.example.android_base_ui
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var timesClicked = 0
-        val btnClickMe = findViewById<Button>(R.id.myButton)
-        val textView = findViewById<TextView>(R.id.textView)
-        btnClickMe.setOnClickListener{
-            timesClicked++
-
-            textView.text = timesClicked.toString()
-
-            Toast.makeText(this, "Hey I'm a toast!", Toast.LENGTH_LONG).show()
+        btnDatePicker.setOnClickListener{ view ->
+            clickDatePicker(view)
         }
+    }
+
+    fun clickDatePicker (view: View) {
+        val myCalendar = Calendar.getInstance()
+        val currentYear = myCalendar.get(Calendar.YEAR)
+        val currentMonth = myCalendar.get(Calendar.MONTH)
+        val currentDay = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                val selectedDate = "$dayOfMonth/${month + 1}/$year"
+                val differenceInMinutes = getDifferenceInMinutes(selectedDate)
+
+                dateText.text = selectedDate
+                minutesText.text = differenceInMinutes.toString()
+            }, currentYear, currentMonth, currentDay
+        )
+
+//      Restrict from selecting future dates and today
+        datePickerDialog.datePicker.maxDate = Date().time - 86400000 // milliseconds of one day
+        datePickerDialog.show()
+    }
+
+    fun getDifferenceInMinutes (date: String): Long {
+//        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+//        val theDate = sdf.parse(selectedDate)
+//        val selectedDateInMinutes = theDate!!.time / 60000
+//        val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+//        val currentDateToMinutes = currentDate!!.time / 60000
+//        val differenceInMinutes = currentDateToMinutes - selectedDateInMinutes
+
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+        val selectedDate = sdf.parse(date)
+        val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+        val currentDateToMinutes = currentDate!!.time / 60000
+        val selectedDateInMinutes = selectedDate!!.time / 60000
+
+        return currentDateToMinutes - selectedDateInMinutes
     }
 }
